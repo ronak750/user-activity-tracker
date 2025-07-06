c223f60ad8b696ee944dc3a3b46da29df0bb6fc8
Ronak Katta <ronakkatta28@Ronaks-MacBook-Air.local>
2025-07-06 11:51:58 +0530

    Trial comit

--- src/main/java/com/example/yesmadam/demo/useractivity/controller/UserDetailsController.java
+++ src/main/java/com/example/yesmadam/demo/useractivity/controller/UserDetailsController.java
@@ -4,8 +4,6 @@ import com.example.yesmadam.demo.useractivity.dto.UserDetailsResponseDTO;
 import com.example.yesmadam.demo.useractivity.exception.InvalidParameterException;
 import com.example.yesmadam.demo.useractivity.model.UserAction;
 import com.example.yesmadam.demo.useractivity.service.UserDetailsService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
-import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -36,15 +33,16 @@ public class UserDetailsController {
 
         // Validate userAction string (to prevent IllegalArgumentException)
         Optional<UserAction> actionEnum = parseUserAction(userAction);
-        if (userAction != null && actionEnum.isEmpty()) {
-            throw new InvalidParameterException("Invalid userAction provided. Allowed values: " + Arrays.toString(UserAction.values()));
-        }
+        // if (userAction != null && actionEnum.isEmpty()) {
+        //     throw new InvalidParameterException("Invalid userAction provided. Allowed values: " + Arrays.toString(UserAction.values()));
+        // }
 
         UserDetailsResponseDTO response = userDetailsService.getUserActions(userId, actionEnum.orElse(null), limit, sortingOrder, pageNo);
         return ResponseEntity.ok(response);
     }
 
     private Optional<UserAction> parseUserAction(String action) {
+        System.out.println("Action: " + action);
         if (action == null) return Optional.empty();
         try {
             return Optional.of(UserAction.valueOf(action.toUpperCase()));
